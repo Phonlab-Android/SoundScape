34911991cadb35f875fe66c591dee96272c737af
Michael Highsmith <michaelhighsmith11@gmail.com>
2019-04-24 19:50:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/phonlab/soundscape/NatureActivity.java
@@ -0,0 +1,15 @@
+package com.example.phonlab.soundscape;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class NatureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nature_layout);
+    }
+
+}
