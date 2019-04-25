21244e38608dc76f91f35a0a1c767ba744764f02
Michael Highsmith <michaelhighsmith11@gmail.com>
2019-04-24 21:12:45 -0500

    debugging

--- app/src/main/java/com/example/phonlab/soundscape/AnimalsActivity.java
+++ app/src/main/java/com/example/phonlab/soundscape/AnimalsActivity.java
@@ -17,8 +17,64 @@ public class AnimalsActivity extends AppCompatActivity {
     }
 
     public void playDog(View view) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+        }
         mMediaPlayer = MediaPlayer.create(this, R.raw.dog_bark);
         mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener(mCompletionListener);
     }
+
+    public void playCat(View view) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+        }
+        mMediaPlayer = MediaPlayer.create(this, R.raw.cat_meow);
+        mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener(mCompletionListener);
+    }
+
+    public void playCow(View view) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+        }
+        mMediaPlayer = MediaPlayer.create(this, R.raw.cow_moo);
+        mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener(mCompletionListener);
+    }
+
+    public void playPig(View view) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+        }
+        mMediaPlayer = MediaPlayer.create(this, R.raw.pig_snort);
+        mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener(mCompletionListener);
+    }
+
+    public void playHorse(View view) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+        }
+        mMediaPlayer = MediaPlayer.create(this, R.raw.horse_neigh);
+        mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener(mCompletionListener);
+    }
+
+    public void playRooster(View view) {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+        }
+        mMediaPlayer = MediaPlayer.create(this, R.raw.rooster_crow);
+        mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener(mCompletionListener);
+    }
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            mMediaPlayer.release();
+        }
+    };
 
 }
