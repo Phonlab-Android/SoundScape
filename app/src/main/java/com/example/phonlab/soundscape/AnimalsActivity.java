7b66227bc429b7d18545a11b657aeeb7ffa61f68
Michael Highsmith <michaelhighsmith11@gmail.com>
2019-04-24 20:58:58 -0500

    MediaPlayer

--- app/src/main/java/com/example/phonlab/soundscape/AnimalsActivity.java
+++ app/src/main/java/com/example/phonlab/soundscape/AnimalsActivity.java
@@ -1,15 +1,24 @@
 package com.example.phonlab.soundscape;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class AnimalsActivity extends AppCompatActivity {
+
+    MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.animals_layout);
+    }
+
+    public void playDog(View view) {
+        mMediaPlayer = MediaPlayer.create(this, R.raw.dog_bark);
+        mMediaPlayer.start();
     }
 
 }
